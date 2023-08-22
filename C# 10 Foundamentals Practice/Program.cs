0898b37e079bcc57e826d26be33225127df464f5
Valentyn Tukov <valentin.tukov@gmail.com>
2023-08-22 17:59:52 +0200

    Sum of ints

--- C# 10 Foundamentals Practice/Program.cs
+++ C# 10 Foundamentals Practice/Program.cs
@@ -6,12 +6,21 @@
 //string surname = Console.ReadLine();
 //Console.WriteLine("Hello, " +  name + " " +surname + "!");
 
-//2. int variable
+//2. int variable + converting
 
+//Console.WriteLine("Type your age");
+//string ageSting = Console.ReadLine();
+//int ageOfUser = int.Parse(ageSting);    
+//Console.WriteLine("You are " + ageOfUser + " years old");
 
-Console.WriteLine("Type your age");
-string ageSting = Console.ReadLine();
-int ageOfUser = int.Parse(ageSting);    
-Console.WriteLine("You are " + ageOfUser + "years old");
+//3. Sum of ints
 
+int a;
+Console.WriteLine("Type the first number");
+a = int.Parse(Console.ReadLine());
+int b;
+Console.WriteLine("Type the second number");
+b = int.Parse(Console.ReadLine());
+int c = a + b;
+Console.WriteLine("The sum is " +  c);
 
