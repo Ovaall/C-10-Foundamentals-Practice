e26a4300890276128eada8fef87a953d24029dc5
Valentyn Tukov <valentin.tukov@gmail.com>
2023-08-22 13:08:39 +0200

    First 2 excersises

--- C# 10 Foundamentals Practice/Program.cs
+++ C# 10 Foundamentals Practice/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Create a greeting
+
+//Console.WriteLine("Type your first name");
+//string name = Console.ReadLine();
+//Console.WriteLine("Type your second name");
+//string surname = Console.ReadLine();
+//Console.WriteLine("Hello, " +  name + " " +surname + "!");
+
+//2. int variable
+
+
+Console.WriteLine("Type your age");
+string ageSting = Console.ReadLine();
+int ageOfUser = int.Parse(ageSting);    
+Console.WriteLine("You are " + ageOfUser + "years old");
+
+
