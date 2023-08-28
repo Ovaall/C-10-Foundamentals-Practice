4bcde889f1444c2744e0fe691cfd6743b5a3193a
Valentyn Tukov <valentin.tukov@gmail.com>
2023-08-28 19:24:15 +0200

    Exercises 4-7

--- C# 10 Foundamentals Practice/Program.cs
+++ C# 10 Foundamentals Practice/Program.cs
@@ -15,12 +15,44 @@
 
 //3. Sum of ints
 
-int a;
-Console.WriteLine("Type the first number");
-a = int.Parse(Console.ReadLine());
-int b;
-Console.WriteLine("Type the second number");
-b = int.Parse(Console.ReadLine());
-int c = a + b;
-Console.WriteLine("The sum is " +  c);
+//int a;
+//Console.WriteLine("Type the first number");
+//a = int.Parse(Console.ReadLine());
+//int b;
+//Console.WriteLine("Type the second number");
+//b = int.Parse(Console.ReadLine());
+//int c = a + b;
+//Console.WriteLine("The sum is " + c);
+
+//4. decimal
+
+//decimal price = 100.0M;
+//Console.WriteLine("The price is equal to " + price);
+
+//5. constants and double
+
+//const double profitMargin = 0.17d;
+//double doublePrice = (double)price;
+//double profit = doublePrice * profitMargin;
+//Console.WriteLine("The profit is equal to " + profit);
+
+
+//6. Compound operator
+
+//Console.WriteLine("Type a number");
+//int a = int.Parse(Console.ReadLine());
+//a += 3;
+//Console.WriteLine("A number after increasing by the compound operator by 3 is equal " + a);
+//Console.ReadLine();
+
+//7. char methods and IsWhiteSpace
+
+//char aChar = 'a';
+//bool isWhiteSpace = char.IsWhiteSpace(aChar);
+//Console.WriteLine($"Character: {aChar}");
+//Console.WriteLine($"IsWhiteSpace: {isWhiteSpace}");
+//Console.ReadLine();
+
+
+
 
